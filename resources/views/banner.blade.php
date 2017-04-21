22f0d9704e54791723acbff33c892a0be36e7ab8
stylder <stylder@gmail.com>
2017-04-21 16:43:27 -0500

    Terminado el efecto

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -4,10 +4,18 @@
 
 <div class="efecto">
     <div id="container" class="">
+        <div class="children-with-img">
+            <img src="images/logo/logo.svg" class="img-responsive img-center-vertical-horizontal">
+        </div>
+
         <div id="output" class="">
+
+
+
         </div>
         <div id="controls" class="controls">
         </div>
+
         <ul  class="">
             <br>
             <br>
